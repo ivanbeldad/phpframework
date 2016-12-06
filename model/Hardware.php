1ff8b6fab9b94236cf2fe83f1644cf4e038fd8c2
Ivan de la Beldad Fernandez <a.ivan@opmbx.org>
2016-12-06 01:22:04 +0100

    Added composer.phar. Modified database to testing. Added harware class to testing.

---
+++ model/Hardware.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: Ivan de la Beldad Fernandez
+ * Date: 05/12/2016
+ * Time: 3:02
+ */
+
+namespace Akimah\Model;
+
+
+class Hardware extends Model
+{
+
+    protected function table(Table &$fields)
+    {
+        $fields->int("id")->autoIncrement()->primaryKey();
+        $fields->string("name", "100");
+        $fields->decimal("price")->defaultValue("100");
+        $fields->string("category")->nullable();
+        $fields->date("date_up");
+        $fields->int("warranty")->nullable();
+    }
+
+    protected function setTableName(&$tableName)
+    {
+        $tableName = "hardware";
+    }
+
+}
